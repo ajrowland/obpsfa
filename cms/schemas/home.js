3b14a0b7cb6fc4e22919d3b59ed75344690d54f6
Andrew Rowland <andrew.rowland@amazerealise.com>
2020-04-29 15:51:40 +0100

    Prevent deletion of home page.

--- cms/schemas/home.js
+++ cms/schemas/home.js
@@ -2,6 +2,7 @@ export default {
   name: 'home',
   title: 'Home',
   type: 'document',
+  __experimental_actions: [/*'create',*/ 'update', /*'delete',*/ 'publish'],
   fields: [
     {
       name: 'title',
