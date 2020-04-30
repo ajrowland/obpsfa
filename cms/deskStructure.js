62a29385a568d9cf3dd8a0f04254595f32b7df70
Andrew Rowland <andrew.rowland@amazerealise.com>
2020-04-30 19:25:06 +0100

    Added preview functionality.

--- cms/deskStructure.js
+++ cms/deskStructure.js
@@ -1,5 +1,25 @@
 import S from "@sanity/desk-tool/structure-builder"
 import MdHome from 'react-icons/lib/md/home'
+import EyeIcon from 'part:@sanity/base/eye-icon'
+import EditIcon from 'part:@sanity/base/edit-icon'
+
+// Web preview
+import IframePreview from './previews/iframe/IframePreview'
+
+// Web preview configuration
+const remoteURL = 'https://some-deployed-dev-site.com'
+const localURL = 'http://localhost:8080'
+const previewURL = window.location.hostname === 'localhost' ? localURL : remoteURL
+
+export const getDefaultDocumentNode = ({schemaType}) => {
+  // Conditionally return a different configuration based on the schema type
+  if (schemaType === "page") {
+    return S.document().views([
+      S.view.form().icon(EditIcon),
+      S.view.component(IframePreview).options({previewURL}).title('Web').icon(EyeIcon)
+    ])
+  }
+ }
 
 export default () =>
   S.list()
---
+++ cms/resolveProductionUrl.js
@@ -0,0 +1,3 @@
+export default function resolveProductionUrl(document) {
+  return `https://obpsfa.andrewrowland.com/${document.slug.current}`
+}
