a6e4e40e580565ec9fe1851fef4071cba0065472
Andrew Rowland <andrew.rowland@kinandcarta.com>
2024-05-25 11:04:34 +0100

    feat: trial form

--- site/src/main.js
+++ site/src/main.js
@@ -48,4 +48,12 @@ export default function(Vue, { router, head, isClient }) {
     rel: "preconnent",
     href: "https://www.googletagmanager.com",
   });
+
+  head.script.push({
+    innerHTML: `(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+        .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+        n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+        (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+        ml('account', '960053');`,
+  });
 }
---
+++ site/src/pages/trial.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout>
+    <div class="trial-form">
+      <div class="ml-embedded" data-form="29OKUs"></div>
+    </div>
+  </Layout>
+</template>
+
+<style lang="scss">
+.trial-form {
+  @include mq($from: tablet) {
+    width: 70%;
+  }
+}
+
+.ml-form-formContent {
+  margin-bottom: 100px !important;
+}
+
+.ml-field-group label {
+  padding: 0;
+}
+</style>
