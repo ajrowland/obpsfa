5bb7b8ed58375fa6b5ee71131dab5c333ca94aeb
Andrew Rowland <andrew.rowland@kinandcarta>
2024-08-11 20:56:33 +0100

    chore: deploy amends

--- README.md
+++ README.md
@@ -4,10 +4,16 @@
 
 Run the Sanity Studio with:
 
-`sanity start`
+`npm run dev`
 
 ## Site
 
 Run the Gridsome site:
 
 `gridsome develop`
+
+## Site (Astro)
+
+Run the Astro site:
+
+`npm run dev`
--- site-astro/src/env.d.ts
+++ site-astro/src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference types="astro/client" />
 /// <reference types="@sanity/astro/module" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_SANITY_PROJECT_ID: string;
+  readonly PUBLIC_SANITY_DATASET: string;
+  readonly SANITY_TOKEN: string;
+  readonly PUBLIC_IMGUR_CLIENT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
